e9612370a86b1f12379e2d06033a905c1a03a2a5
student <dorian.pettway-bowman@ngc.com>
2021-02-21 16:25:40 +0000

    Add logic and test

--- src/test/java/com/piedpiper/Bookzy/BookzyApplicationTests.java
+++ src/test/java/com/piedpiper/Bookzy/BookzyApplicationTests.java
@@ -1,13 +1,18 @@
 package com.piedpiper.Bookzy;
 
-import org.junit.jupiter.api.Test;
+package com.piedpiper.Bookzy;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookzyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    private BookzyApplication analyser = new BookzyApplication();
+    @Test	 
+    public void testWordCount() {     	 
+   	 assertEquals(7,analyser.getWordCount("Train to win in the digital economy"));	 
+    }
 }
+
